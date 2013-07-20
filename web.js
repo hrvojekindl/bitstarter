327d378b34560ddc1bdeff21929583bdde876322
Hrvoje Kindl <hrvoje.kindl@gmail.com>
2013-07-20 12:02:42 +0200

    gitignore

--- web.js
+++ web.js
@@ -5,7 +5,6 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  // response.send('Hello World 2!');
   response.send(fs.readFileSync('index.html').toString());
 });
 
